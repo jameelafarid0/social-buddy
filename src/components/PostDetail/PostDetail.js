88d5832a967d34fb32b40a39faacf769ea184477
jameelafarid0 <jameelafarid9@gmail.comm>
2020-09-06 17:33:58 +0300

    done assignment social network

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Comments from '../Comments/Comments';
 
